f83d71db8166ab02eeecce9abc55b80815e8c3fc
Kunju Perath <kperath@gmail.com>
2019-09-16 23:48:24 -0400

    Finished test mail. Problem with jar file. Need to add comments.

--- src/MailApp.java
+++ src/MailApp.java
@@ -1,13 +1,18 @@
 import java.util.Properties;
-import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.InternetAddress;
+import javax.mail.Transport;
 
 public class MailApp {
     public static void main(String[] args) {
         final String host = "smtp.gmail.com";
         final String username = System.getenv("MAIL_USERNAME"); // the username
         final String password = System.getenv("MAIL_PASSWORD"); // the password
-        String to = System.getenv("MAIL_USERNAME"); // sending an email to oneself
+        String recipient = System.getenv("MAIL_USERNAME"); // sending an email to oneself
 
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -24,6 +29,26 @@ public class MailApp {
             }
                  }
         );
+
+        try {
+            Message message = new MimeMessage(session);
+
+            InternetAddress recipientAddress = new InternetAddress(recipient);
+
+            message.setFrom(new InternetAddress(username));
+            message.setRecipient(Message.RecipientType.TO, recipientAddress);
+
+            message.setSubject("JavaMail API test");
+            message.setText("Testing 123");
+
+            Transport.send(message);
+
+            System.out.println("Message sent!");
+
+        }
+        catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
